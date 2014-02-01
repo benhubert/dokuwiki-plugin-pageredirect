3a38f0af56ece9b9d2751bcbaa14ff489980faba
Benjamin Plocek <plocek@artindustrial.com>
2014-02-01 23:27:17 +0100

    added german (informal) translation

---
+++ lang/de-informal/lang.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * German language for Page Redirect plugin
+ *
+ * @license:    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author:     Benjamin Plocek <code@benjaminplocek.com>
+ */
+
+$lang['redirect_to']     = "Diese Seite wurde verschoben, der neue Name lautet %s.";
+$lang['redirected_from'] = "Du wurdest von %s auf diese Seite weitergeleitet.";
