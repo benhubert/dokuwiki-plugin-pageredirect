26abf14fc8ec04c492f0c14112de3160cf8ced3e
Benjamin Plocek <plocek@artindustrial.com>
2014-02-01 23:26:00 +0100

    added german translation

---
+++ lang/de/settings.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * German language for Page Redirect plugin
+ *
+ * @license:    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author:     David Lorentsen <zyberdog@quakenet.org>
+ */
+
+$lang['show_note'] = 'Benutzer darauf hinweisen, wenn er/sie automatisch auf eine Seite weitergeleitet wurde?';
